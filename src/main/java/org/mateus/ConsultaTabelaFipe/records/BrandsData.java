cfc118e4fafd42c208195dd6aa3bf342b4a30c44
Mateus Cardoso <cardoso.mateusv@gmail.com>
2024-10-08 20:23:51 -0300

    Update BrandsData.java

--- src/main/java/org/mateus/ConsultaTabelaFipe/records/BrandsData.java
+++ src/main/java/org/mateus/ConsultaTabelaFipe/records/BrandsData.java
@@ -5,6 +5,6 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record BrandsData(
-        @JsonAlias("") Integer code,
-        @JsonAlias("") String brand
+        @JsonAlias("codigo") String code,
+        @JsonAlias("nome") String brand
 ) {}
