1947c43eba984c4638698a4bcec31a7ba69dde39
Mateus Cardoso <cardoso.mateusv@gmail.com>
2024-10-08 20:14:00 -0300

    Update ResponseHandler.java

--- src/main/java/org/mateus/ConsultaTabelaFipe/handler/ResponseHandler.java
+++ src/main/java/org/mateus/ConsultaTabelaFipe/handler/ResponseHandler.java
@@ -1,10 +1,17 @@
 package org.mateus.ConsultaTabelaFipe.handler;
 
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class ResponseHandler implements IResponseHandler {
     @Override
     public <T> T consumeResponse(String json, Class<T> tclass) {
-
-        return null;
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            return mapper.readValue(json, tclass);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
