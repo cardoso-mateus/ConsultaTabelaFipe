952ad6663abe17b676a7857f3c26f13920433874
Mateus Cardoso <cardoso.mateusv@gmail.com>
2024-10-08 20:05:31 -0300

    Create ResponseHandler.java

---
+++ src/main/java/org/mateus/ConsultaTabelaFipe/handler/ResponseHandler.java
@@ -0,0 +1,10 @@
+package org.mateus.ConsultaTabelaFipe.handler;
+
+
+public class ResponseHandler implements IResponseHandler {
+    @Override
+    public <T> T consumeResponse(String json, Class<T> tclass) {
+
+        return null;
+    }
+}
