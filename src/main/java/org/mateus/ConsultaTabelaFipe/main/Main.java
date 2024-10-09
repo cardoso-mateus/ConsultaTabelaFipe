44d981a206053eaa3f8ea78ba72dd58a3f82e5a2
Mateus Cardoso <cardoso.mateusv@gmail.com>
2024-10-09 20:07:55 -0300

    Update Main.java

--- src/main/java/org/mateus/ConsultaTabelaFipe/main/Main.java
+++ src/main/java/org/mateus/ConsultaTabelaFipe/main/Main.java
@@ -3,8 +3,11 @@ package org.mateus.ConsultaTabelaFipe.main;
 import org.mateus.ConsultaTabelaFipe.consumer.APIConsumer;
 import org.mateus.ConsultaTabelaFipe.handler.ResponseHandler;
 import org.mateus.ConsultaTabelaFipe.records.BrandsData;
-import org.mateus.ConsultaTabelaFipe.records.ModelsData;
+import org.mateus.ConsultaTabelaFipe.records.ModelsSetData;
+import org.mateus.ConsultaTabelaFipe.records.VehicleData;
+import org.mateus.ConsultaTabelaFipe.records.YearsData;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -19,17 +22,52 @@ public class Main {
     public void start() {
         String url = null;
         url = selectVehicleType(url);
+        url = selectVehicleBrand(url);
+        url = selectModel(url);
+        selectModelYear(url);
+    }
 
-        System.out.println("\nEscolha uma marca de acordo com o código ao lado");
+    private void selectModelYear(String url) {
+        System.out.println("\nEscolha o ano do modelo de acordo com o código ao lado");
+        String choice = input.nextLine();
+
+        url = url + choice;
+
+        String json = consumer.APIConsume(url);
+        VehicleData vehicleData = handler.consumeResponse(json, VehicleData.class);
+        System.out.println(vehicleData);
+    }
+
+    private String selectModel(String url) {
+        System.out.println("\nEscolha um modelo de acordo com o código ao lado");
         int choice = input.nextInt();
+        input.nextLine();
+
+        url = url + choice + "/anos/";
+
+        String json = consumer.APIConsume(url);
+
+        List<YearsData> yearsData = List.of(handler.consumeResponse(json, YearsData[].class));
+        yearsData.forEach(System.out::println);
+
+        return url;
+    }
+
+    private String selectVehicleBrand(String url) {
+        System.out.println("\nEscolha uma marca de acordo com o código ao lado");
+            int choice = input.nextInt();
         input.nextLine();
 
         url = url + choice + "/modelos/";
 
         String json = consumer.APIConsume(url);
+        ModelsSetData modelsSetData = handler.consumeResponse(json, ModelsSetData.class);
 
-        List<ModelsData> brandsData = List.of(handler.consumeResponse(json, ModelsData[].class));
-        brandsData.forEach(System.out::println);
+        List<ModelsSetData> modelsSetDataList = new ArrayList<>();
+        modelsSetDataList.add(modelsSetData);
+        modelsSetDataList.stream().flatMap(m -> m.models().stream()).forEach(System.out::println);
+
+        return url;
     }
 
     private String selectVehicleType(String url) {
