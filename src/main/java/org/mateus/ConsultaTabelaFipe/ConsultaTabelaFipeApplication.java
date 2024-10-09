2ac0e66a5098fd27e0d372e4819d814ff7e3b63f
Mateus Cardoso <cardoso.mateusv@gmail.com>
2024-10-09 01:15:06 -0300

    Update ConsultaTabelaFipeApplication.java

--- src/main/java/org/mateus/ConsultaTabelaFipe/ConsultaTabelaFipeApplication.java
+++ src/main/java/org/mateus/ConsultaTabelaFipe/ConsultaTabelaFipeApplication.java
@@ -1,13 +1,9 @@
 package org.mateus.ConsultaTabelaFipe;
 
-import org.mateus.ConsultaTabelaFipe.consumer.APIConsumer;
-import org.mateus.ConsultaTabelaFipe.handler.ResponseHandler;
-import org.mateus.ConsultaTabelaFipe.records.BrandsData;
+import org.mateus.ConsultaTabelaFipe.main.Main;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.List;
 
 @SpringBootApplication
 public class ConsultaTabelaFipeApplication implements CommandLineRunner {
@@ -18,10 +14,7 @@ public class ConsultaTabelaFipeApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		APIConsumer consumer = new APIConsumer();
-		ResponseHandler handler = new ResponseHandler();
-		String json = consumer.APIConsume("https://parallelum.com.br/fipe/api/v1/carros/marcas");
-		List<BrandsData> brandsData = handler.consumeResponse(json, List.class);
-    	System.out.println(brandsData);
+		Main main = new Main();
+		main.start();
 	}
 }
